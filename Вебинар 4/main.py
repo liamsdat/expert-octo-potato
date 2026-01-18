42e85c7870b0653ada4861bc0625060597156c10
liamsdat <liamsdat@icloud.com>
2026-01-18 02:51:38 -0800

    Добавил логику с детьми

--- Вебинар 4/main.py
+++ Вебинар 4/main.py
@@ -3,14 +3,15 @@ spb_price = 10_000
 ekb_price = 8_000  # Можно добавить _ в числе и python будет это игнорировать
 
 dist = input("Введите пункт назначения (msc, spb, ekb): ")
-count = int(input("Введите количество людей: "))
+adults = int(input("Введите количество взрослых: "))
+kids = int(input("Введите количество детей: "))
 
 if dist == "msc":
-    price = msc_price * count
+    price = msc_price * (2 * adults + kids) // 2
 elif dist == "spb":
-    price = spb_price * count
+    price = spb_price * (2 * adults + kids) // 2
 elif dist == "ekb":
-    price = ekb_price * count
+    price = ekb_price * (2 * adults + kids) // 2
 
-print(f"Итоговая цена: {price}")
+print("Цена поездки: ", price)
 
